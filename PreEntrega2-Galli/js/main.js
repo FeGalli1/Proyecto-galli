84cb4924ed17e4587a40999c3ade71cfa665436c
bucata <felipegall1.fg@gmail.com>
2023-05-03 22:30:44 -0300

    Subo la preentrega 3

--- PreEntrega2-Galli/js/main.js
+++ PreEntrega2-Galli/js/main.js
@@ -1,197 +1,236 @@
-//VARIABLES GLOBALES
-let user=1;
-let base= Math.round(Math.random() * 1000000);//maximo de productos admitidos 1000000. este seria como su id
-let codigos=0;
-///////////////////////
-
-//CLASES//////////////
-class producto{
-    //constructor
-    constructor(nombre,stock=0 , marca="generica"){//constructor parametrizado
-        this.marca=marca;
-        this.nombre=nombre;
-        if(isNaN(stock)==false && parseInt(stock)>=0)
+    // Variables
+    const baseDeDatos = [
         {
-            this.stock=parseInt(stock);
-        }else{
-            this.stock=0;
-        }
-        this.codigo = base+codigos;
-        codigos++;
-        this.actualizacion = new Date();//solamente se almacena la ultima vez que se agrego stock.
-    }
-
-    imprimirConsola(){
-        console.log("producto: "+this.nombre  +", Marca: "+ this.marca +", stock: "+ this.stock  +" , id: "+ this.codigo +", ultima actualizacion: "+ this.actualizacion.toLocaleString());
-    }
-}
-
-
-///////////////////////////////////////////////
-//ARRAYS//////////////
-const gondola = [
-    new producto ("ps5",12,"sony") , 
-    new producto ("ps4",2,"sony"),
-    new producto ("gameboy",6,"nintendo"),
-    new producto ("notebook",2,"asus"),
-    new producto ("notebook",2,"hp"),
-    new producto ("joystick",10),//si no le pongo marca por defecto deberia ser "generico". eso esta definido en  el constructor
-    new producto ("auriculasres",2,"jbl"),
-    new producto ("parlante",2,"jbl"),
-    new producto ("joystick",6,"sony"),
-];
-
-//////////////////////
-/////////////////FUNCIONES/////////////////////
-
-function error(codigoError)
-{
-    switch (codigoError) {
-        case 1:
-            console.log("ERROR EN EL INGRESO DE DATOS DE LA BUSQUEDA. \n CODIGO DE ERROR: "+1);
-            break;
-        case 2:
-            console.log("ERROR!! NO RECONOCE EL VALOR COMO UN NUMERO. \n CODIGO DE ERROR: "+2);
-            break;
-        default:
-            break;
-    }
-}
-
-function menuOpciones(opcion)
-{
-    let retorno=1,modifica=0;
-    let mensaje, resultado;
-    switch (opcion) {
-        case "stock":
-            mensaje= nuevoDato("igrese el nombre o la marca del producto que quiere consultar, si desea ver todos solo igrese todos");
-            if(mensaje !== "todos" && mensaje !== "todo" )
-            {
-                const resultado = gondola.filter( producto => producto.nombre === mensaje || producto.marca === mensaje );
-                console.table(resultado); 
-            
-            
-            }else if(mensaje == "todos" || mensaje == "todo" ){
-                const resultado = gondola.filter( producto => producto.stock !== -1 );
-                console.table(resultado); 
-            }else{
-                error(1);
-            }
-            break;
-        case "poner":
-            
-             mensaje = nuevoDato("igrese el nombre o la marca del producto al que quiere agregar mercaderia");
-             resultado = gondola.filter( producto => producto.nombre === mensaje || producto.marca === mensaje );
-            console.table(resultado);
-            if(resultado.length>1)
-            {
-                
-                
-                let marca = nuevoDato("se encontraron mas de un producto con esa carracteristica, le voy a pedir que ingrese SOLO la marca");
-                let nombre =nuevoDato("ahora ingrese el nombre");
-                resultado = gondola.findIndex( producto => producto.nombre === nombre && producto.marca === marca );
-                console.table(resultado);
-                
-                if( isNaN(mensaje = nuevoDato("¿Cuanto desea agregar?"))==false)
-                {
-                    gondola[resultado].stock += parseInt(mensaje);
-                    gondola[resultado].actualizacion =  new Date();
-                    console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                }else{
-                    error(2);
-                }
-            }else{
-                resultado = gondola.findIndex( producto => producto.nombre === mensaje || producto.marca === mensaje );
-                if( isNaN(mensaje = nuevoDato("¿Cuanto desea agregar?"))==false)
-                {
-                    gondola[resultado].stock += parseInt(mensaje);
-                    gondola[resultado].actualizacion =  new Date();
-                    console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                }else {
-                    error(2);
-                }
-            }
-            break;
-        case "sacar":
-             mensaje = nuevoDato("igrese el nombre o la marca del producto al que quiere sacar mercaderia");
-             resultado = gondola.filter( producto => producto.nombre === mensaje || producto.marca === mensaje );
-            console.table(resultado);
-            if(resultado.length>1)
-            {
-                let marca = nuevoDato("se encontraron mas de un producto con esa carracteristica, le voy a pedir que ingrese SOLO la marca");
-                let nombre =nuevoDato("ahora ingrese el nombre");
-                resultado = gondola.findIndex( producto => producto.nombre === nombre && producto.marca === marca );
-                console.table(resultado);
-                
-                if( isNaN(mensaje = nuevoDato("¿Cuanto desea sacar?"))==false)
-                {
-                    if(gondola[resultado].stock >= parseInt(mensaje))
-                    {
-                        gondola[resultado].stock -= parseInt(mensaje);
-                        console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                    }else{
-                        error(3);
-                        gondola[resultado].stock = 0;
-                        console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                    }
-                }else{
-                    error(2);
-                }
-            }else{
-                resultado = gondola.findIndex( producto => producto.nombre === mensaje || producto.marca === mensaje );
-                if( isNaN(mensaje = nuevoDato("¿Cuanto desea sacar?"))==false)
-                {
-                    if(gondola[resultado].stock >= parseInt(mensaje))
-                    {
-                        gondola[resultado].stock -= parseInt(mensaje);
-                        console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                    }else{
-                        error(3);
-                        gondola[resultado].stock = 0;
-                        console.log("nuevo valor de stock = "+ gondola[resultado].stock);
-                    }
-                }else {
-                    error(2);
-                }
-            }
-            
-            break;
-        case "terminar":
-            retorno=0;
-            break;
-        case "ayuda":
-            alert("Lista de comandos: "+ '\n' + " \"stock\" : Te dira el stock disponible de x producto" + '\n' + "\"poner\" : Para agregar \"X\" stock" + '\n' + "\"sacar\" : Para retirar cantidad \"X\" de stock" + '\n' + "\"terminar\" : Para terminar el proceso");
-            break;
-        default:
-            //EN CASO QUE EL VALOR NO SEA VALIDO
-            alert("opcion no valida, ingrese \"ayuda\" para obtener informacion de comandos");    
-        break;
-    }
-    return retorno;
-}
-
-function nuevoDato(dato = "Ingrese Dato"){
-    //RECIBIMOS LA OPERACION QUE SE DESEA REALIZAR
-    let retorno = prompt(dato);
-    //CONVERTIMOS EL STRING EN MINUSCULA PARA ACEPTAR PALABRAS CON MAYUSCULA SIN PROBLEMA
-    retorno = retorno.toLowerCase();
-    //DEVOLVEMOS LA PALABRA INGRESADA EN MINUSCULA LISTA PARA USARSE
-    return retorno;
-}
-
-//////////////////////////////////////////////
-
-
-
-
-
-
-
-//while(1){//siempre es verdadero solo salgo con un break
-
-
-//FUNCION PRINCIPAL, PIDE EL DATO Y PROCESA. EL WHILE FUNCIONA DURA HASTA QUE EL USUARIO DECIDA TERMINAR
-        while(user!=0)
+            id: 1,
+            nombre: 'Pexels Steve Johnson',
+            precio: 15000,
+            imagen: 'Cuadros/pexels-steve-johnson.jpg',
+            descripcon: 'Arte moderno virtual. Esta obra fue publicada el 9 de agosto del 2021 por Steve Johnson en "pexels". Fue diseñada virtualmente con el software "Adobe Photoshop". '
+        },
         {
-            user=menuOpciones(nuevoDato('Ingrese una operacion ("stock" "poner" "sacar" "ayuda" "terminar"'));
+            id: 2,
+            nombre: 'Costa En Santa Cristina',
+            precio: 94000,
+            imagen: 'Cuadros/COSTA EN SANTA CRISTINA.jpg',
+            descripcon: 'Una obra del autor "Joaquin Sorolla Y Bastida" y fue hecha en el año 1914. Esta hecha con la tecnica de óleo sobre lienzo y tiene una dimension de 99x74 cm'
+        },
+    
+        {
+            id: 3,
+            nombre: 'Tintas Tintas y Tintas',
+            precio: 10000,
+            imagen: 'Cuadros/Tintas-tintas-y-tintas.jpg',
+            descripcon: 'Una obra de la autora "Luchi Sanguinetti". El color y la forma prevalecen en mi visión. Aparece reiteradamente formas percibidas en el pasado, las cuales han dejado huella en mi memoria. Hay trazo que se repite y la lectura es múltiple. Realidades con diversas percepciones.'
+        },
+        {
+            id: 4,
+            nombre: 'On The Road From Moret',
+            precio: 11000,
+            imagen: 'Cuadros/Alfred Sisley On the Road from Moret.jpg',
+            descripcon: 'Una obra del autor "Alfred Sisley" y fue hecha en el año 1882.El dibujo lleva una inscripción que identifica el lugar como el camino de Moret a Sainte-Mammès. Tiene un tamaño de 54 x 73 cm'
+            
+        }       
+    ];
+
+    //Array del carrito que empieza vacio
+    let carrito = [];
+    //la divisa que usaremos, tambien debe cambiarse en el html para mostrar (dollar o euro)
+    const divisa = '$';
+    //Buscamos los id y los guardamos
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const miLocalStorage = window.localStorage;
+
+    // Funciones
+
+
+    //Esta funcion muestra los productos a partir de la palabra que se haya ingresado, de no ingresar nada se mostrara todo.
+    function renderizarProductos(palabraClave) {
+        const listaProductos = document.getElementById("items");
+
+        
+        // Vaciar contenido del elemento HTML de esta manera cada vez que la funcion se llame se limpie lo anterior y no se vaya acumulando
+        listaProductos.innerHTML = "";
+
+        //busca por todo el array si encuentra coicidencia con la palabra clave
+          const productosFiltrados = buscarProductos(palabraClave);
+          
+          productosFiltrados.forEach((info) => {
+          // Estructura
+          const miNodo = document.createElement('div');
+          miNodo.classList.add('col','card', 'shadow-ms');
+          const miNodoImagen = document.createElement('img');
+          miNodoImagen.classList.add('img-fluid');
+      
+          miNodoImagen.setAttribute('src', info.imagen);
+      
+          // Body
+          const miNodoCardBody = document.createElement('div');
+          miNodoCardBody.classList.add('card-body');
+          // Titulo
+          const miNodoTitle = document.createElement('h5');
+          miNodoTitle.classList.add('card-title');
+          miNodoTitle.textContent = info.nombre;
+          // Imagen
+      
+          // Precio
+          const miNodoDescripcion = document.createElement('p');
+          miNodoDescripcion.classList.add('card-text');
+          miNodoDescripcion.textContent = `${info.descripcon}`;
+          // Boton 
+          const miNodoBoton = document.createElement('button');
+          miNodoBoton.classList.add('btn', 'btn-primary');
+          miNodoBoton.textContent = '+';
+          miNodoBoton.setAttribute('marcador', info.id);
+          miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+          const miNodoPrecio = document.createElement('p');
+         
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoDescripcion);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
+    }
+
+    
+    //Evento para añadir un producto al carrito de la compra
+    function anyadirProductoAlCarrito(evento) {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(evento.target.getAttribute('marcador'))
+        // Actualizamos el carrito 
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
+    }
+
+    
+    //Dibuja todos los productos guardados en el carrito
+    
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.textContent = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-8');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-8');
+            miBoton.textContent = 'X';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+        // Renderizamos el precio total en el HTML
+        DOMtotal.textContent = calcularTotal();
+    }
+
+    
+    // Evento para borrar un elemento del carrito
+    
+    function borrarItemCarrito(evento) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = evento.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
+
+    }
+
+
+     // Calcula el precio total teniendo en cuenta los productos repetidos
+    function calcularTotal() {
+        // Recorremos el array del carrito 
+        return carrito.reduce((total, item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Los sumamos al total
+            return total + miItem[0].precio;
+        }, 0).toFixed(2);
+    }
+
+    
+    // Varia el carrito y vuelve a dibujarlo
+    
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        // Borra LocalStorage
+        localStorage.clear();
+
+    }
+
+    //guardamos el carrito en caso de que se recargue la pagina
+    function guardarCarritoEnLocalStorage () {
+        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    //vemos si existe algo en mi storage y lo cargo en mi carrito nuevamente
+    function cargarCarritoDeLocalStorage () {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (miLocalStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
         }
+    }
+
+        //esta fucnion busca en la base de datos la palabra clave, si no recibe nada es que se ejecuto por primera vez o con el bucador vacio y devolvemos todos los productos
+    function buscarProductos(criterio) {
+        if (!criterio) {
+          // Si no hay criterio de búsqueda, devolvemos el array original
+          return baseDeDatos;
+        } else {
+          // Convertimos el criterio a minúsculas para comparar sin importar las mayúsculas
+          const criterioMinusculas = criterio.toLowerCase();
+          // Filtramos los elementos del array que contengan el criterio de búsqueda
+          const resultadoBusqueda = baseDeDatos.filter((producto) => {
+            return (
+              producto.nombre.toLowerCase().includes(criterioMinusculas) ||
+              (producto.descripcon &&
+                producto.descripcon.toLowerCase().includes(criterioMinusculas))
+            );
+          });
+          return resultadoBusqueda;
+        }
+      }
+      
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
